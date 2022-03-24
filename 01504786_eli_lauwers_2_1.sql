0445fe8c484efad49d0dcb16611f4547dedf40d6
Eli Lauwers <eli.lauwers@be-mobile.com>
2022-03-24 11:28:57 +0100

    update

--- 01504786_eli_lauwers_2_1.sql
+++ 01504786_eli_lauwers_2_1.sql
@@ -2,4 +2,3 @@ SELECT r.license_plate,
     MAX(r.period_begin) - MIN(r.period_begin) passed_nights 
 FROM registration r 
 GROUP BY r.license_plate
-HAVING COUNT(r.email) > 1 -- Excluce vehicles that were only rented once
